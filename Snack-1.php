92c7c7641fdd210e4d100d44e53ec01fc334189f
fabiochidda <fabio.chidda99@gmail.com>
2022-04-04 14:07:42 +0200

    Aggiunto array di partite

---
+++ Snack-1.php
@@ -0,0 +1,37 @@
+<?php
+
+$partite = [
+    [
+        'casa' => 'Roma',
+        'ospite' => 'Latina',
+        'punti_casa' => '22',
+        'punti_ospite' => '17',
+    ],
+    [
+        'casa' => 'Milano',
+        'ospite' => 'Firenze',
+        'punti_casa' => '42',
+        'punti_ospite' => '35',
+    ],
+    [
+        'casa' => 'Palermo',
+        'ospite' => 'Catania',
+        'punti_casa' => '18',
+        'punti_ospite' => '94',
+    ],
+]
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack-1</title>
+</head>
+<body>
+    
+</body>
+</html>
